997a507db3bf3c6400c47421ed38702a3efa8c52
Isiyaku477 <120345128+Isiyaku477@users.noreply.github.com>
2026-02-10 01:07:13 +0100

    Add sidebar toggle functionality

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("Frontend tasks loaded");
+const sidebar = document.getElementById("sidebar");
+const toggleBtn = document.getElementById("toggleBtn");
+
+toggleBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("collapsed");
+});
